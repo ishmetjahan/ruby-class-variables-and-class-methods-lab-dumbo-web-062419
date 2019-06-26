f08378f1d636e71fbe675c494c804aaa04198825
Ishmet <ishmetjahan@yahoo.com>
2019-06-26 21:45:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,10 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
-  def initiazlie  (name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-  
+  def new (name, artist, genre)
+    @name= name
+    @artist= artist
+    @genre= genre
   end
 
 end
